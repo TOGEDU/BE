5b2bf7fa3153f8225f8402541fa7a44f71bc5880
soyoon1 <20210841@duksung.ac.kr>
2024-04-29 19:06:16 +0900

    [0429] 프로젝트 생성

---
+++ togedu/src/main/java/passion/togedu/TogeduApplication.java
@@ -0,0 +1,13 @@
+package passion.togedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogeduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogeduApplication.class, args);
+	}
+
+}
---
+++ togedu/src/test/java/passion/togedu/TogeduApplicationTests.java
@@ -0,0 +1,13 @@
+package passion.togedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
