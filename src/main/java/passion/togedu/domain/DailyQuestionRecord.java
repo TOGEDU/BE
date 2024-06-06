d1bfd5b7ddae04f506d80f5627f0cecba9795a70
soyoon1 <20210841@duksung.ac.kr>
2024-06-06 17:03:57 +0900

    Feat: 부모, 음성기록, 오늘의 질문 관련 엔티티 생성

---
+++ src/main/java/passion/togedu/domain/DailyQuestionRecord.java
@@ -0,0 +1,39 @@
+package passion.togedu.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class DailyQuestionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @CreatedDate
+    private LocalDateTime date;
+
+    @Column(length = 500)
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Parent parent;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", referencedColumnName = "id")
+    private DailyQuestion dailyQuestion;
+
+}
---
+++ src/main/java/passion/togedu/domain/Parent.java
@@ -0,0 +1,47 @@
+package passion.togedu.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.List;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Parent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(length = 100)
+    private String email;
+
+    @Column(length = 500)
+    private String profileImageUrl;
+
+    private Boolean pushStatus;
+
+    private LocalTime pushNotificationTime;
+
+    @Column(length = 50)
+    private String password;
+
+    @Column(length = 50)
+    private String parentCode;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "parent")
+    private List<VoiceRecordingRecord> voiceRecordingRecordList;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "parent")
+    private List<DailyQuestionRecord> dailyQuestionRecordList;
+
+}
