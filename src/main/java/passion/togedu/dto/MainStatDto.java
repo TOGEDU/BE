6a9434647f00af999cdc9e3f7f8180688671dbf1
Sowon <cherishwish12@gmail.com>
2024-06-10 15:07:58 +0900

    Feat: MainStat관련 서비스 컨트롤러 레포지토리 생성

---
+++ src/main/java/passion/togedu/dto/MainStatDto.java
@@ -0,0 +1,14 @@
+package passion.togedu.dto;
+
+public class MainStatDto {
+    //메인화면 UserStat에 대한 정보
+    private Integer answerCount;
+    private Integer recordPer;
+    private Integer dataPer;
+    private Integer diaryCount;
+
+    //오늘의 질문에 대한 정보?
+    private String Question;
+
+
+}
