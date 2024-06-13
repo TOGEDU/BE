51f5af87934eb8d309462a06b07b5399fcb513ec
Sowon <cherishwish12@gmail.com>
2024-06-13 16:43:05 +0900

    Style: Service 함수 추가

--- src/main/java/passion/togedu/domain/Diary.java
+++ src/main/java/passion/togedu/domain/Diary.java
@@ -17,7 +17,6 @@ public class Diary {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "diary_id")
     private int id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -33,23 +32,4 @@ public class Diary {
     @Column(name = "img_url", length = 500)
     private String imgUrl;
 
-    // 기본 생성자
-//    protected Diary() {
-//    }
-
-    // 필드를 설정할 수 있는 생성자
-    public Diary(ParentChild parentChild, LocalDateTime date, String text, String imgUrl) {
-        this.parentChild = parentChild;
-        this.date = date;
-        this.text = text;
-        this.imgUrl = imgUrl;
-    }
-
-    // 연관관계 메서드(육아일기 기록/삭제할때 연관관계 일관되게 유지 -> 데이터 무결성)
-    public void setParentChild(ParentChild parentChild) {
-        this.parentChild = parentChild;
-        if (parentChild != null && !parentChild.getDiaries().contains(this)) {
-            parentChild.getDiaries().add(this);
-        }
-    }
 }
--- src/main/java/passion/togedu/domain/ParentChild.java
+++ src/main/java/passion/togedu/domain/ParentChild.java
@@ -18,7 +18,6 @@ public class ParentChild {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "parent_child_id")
     private int id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -32,32 +31,8 @@ public class ParentChild {
     @Column(name = "unique_code")
     private String uniqueCode;
 
-    /*@OneToMany(mappedBy = "parentChild", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Diary> diaries = new ArrayList<>();*/
-    @OneToMany(mappedBy = "parentChild") private List<Diary> diaries;
+    @OneToMany(mappedBy = "parentChild")
+    private List<Diary> diaries;
 
-
-/*
-    // 기본 생성자
-//    protected ParentChild() {
-//    }
-
-    // 필드를 설정할 수 있는 생성자
-    public ParentChild(Parent parent, Child child, String uniqueCode) {
-        this.parent = parent;
-        this.child = child;
-        this.uniqueCode = uniqueCode;
-    }
-
-    // 연관관계 메서드
-    public void addDiary(Diary diary) {
-        diaries.add(diary);
-        diary.setParentChild(this);
-    }
-
-    public void removeDiary(Diary diary) {
-        diaries.remove(diary);
-        diary.setParentChild(null);
-    }*/
 }
 
