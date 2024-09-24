9c8df755463147993290223e2e638954a09a863d
soyoon1 <20210841@duksung.ac.kr>
2024-09-24 18:40:17 +0900

    Feat: LLM 답변 사용자별로 다르게 하기 위해 FastAPI 호출 시 JWT 토큰을 헤더에 추가하도록 수정

--- src/main/java/passion/togedu/controller/ChatMessageController.java
+++ src/main/java/passion/togedu/controller/ChatMessageController.java
@@ -1,5 +1,6 @@
 package passion.togedu.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import passion.togedu.dto.chat.ChatMessageRequestDto;
 import passion.togedu.dto.chat.ChatMessageResponseDto;
+import passion.togedu.jwt.SecurityUtil;
 import passion.togedu.service.ChatMessageService;
-import passion.togedu.service.ChatRoomService;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,9 +24,13 @@ public class ChatMessageController {
     private final ChatMessageService chatMessageService;
     //채팅 추가하고 답변 받기
     @PostMapping("")
-    public ResponseEntity<ChatMessageResponseDto> addMessage(@RequestBody ChatMessageRequestDto chatMessageRequestDto) {
+    public ResponseEntity<ChatMessageResponseDto> addMessage(HttpServletRequest request, @RequestBody ChatMessageRequestDto chatMessageRequestDto) {
         try {
-            ChatMessageResponseDto responseDto = chatMessageService.addMessage(chatMessageRequestDto);
+            String jwtToken = SecurityUtil.resolveToken(request);
+            if (jwtToken == null) {
+                throw new RuntimeException("사용자의 JWT 토큰 정보를 찾을 수 없습니다.");
+            }
+            ChatMessageResponseDto responseDto = chatMessageService.addMessage(jwtToken, chatMessageRequestDto);
             return ResponseEntity.ok(responseDto);
         } catch (IllegalArgumentException e) {
             log.error("Error adding chat message: {}", e.getMessage());
