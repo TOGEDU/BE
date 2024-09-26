db6ee339d1109255bc3d92239262eeed614f21c5
soyoon1 <20210841@duksung.ac.kr>
2024-09-26 21:00:35 +0900

    Fix: 채팅메시지 생성 관련 FastAPI api 수정

--- src/main/java/passion/togedu/service/ChatMessageService.java
+++ src/main/java/passion/togedu/service/ChatMessageService.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import passion.togedu.domain.ChatMessage;
 import passion.togedu.domain.ChatRoom;
-import passion.togedu.dto.chat.ChatMessageHistoryDto;
 import passion.togedu.dto.chat.ChatMessageRequestDto;
 import passion.togedu.dto.chat.ChatMessageResponseDto;
 import passion.togedu.repository.ChatMessageRepository;
@@ -64,23 +63,8 @@ public class ChatMessageService {
             // HTTP client 만들기
             HttpClient client = HttpClient.newHttpClient();
 
-            // 채팅방 내역 가져오기
-            List<ChatMessageHistoryDto> chatMessageHistoryDtoList = getChatHistory(chatRoomId);
-
-            // 채팅 내역을 하나의 문자열로 변환
-            StringBuilder chatHistoryBuilder = new StringBuilder();
-            for (ChatMessageHistoryDto historyDto : chatMessageHistoryDtoList) {
-                chatHistoryBuilder.append(historyDto.getRole()).append(": ").append(historyDto.getMessage()).append("\n");
-            }
-            String chatHistory = URLEncoder.encode(chatHistoryBuilder.toString(), StandardCharsets.UTF_8);
-
-            // JSON 형태로 만들기
-            ObjectMapper objectMapper = new ObjectMapper();
-            ObjectNode json = objectMapper.createObjectNode();
-            json.put("message", chatMessage);
-
             String urlWithParams = FASTAPI_URL + "/chat?message=" + URLEncoder.encode(chatMessage, StandardCharsets.UTF_8)
-                    + "&history=" + chatHistory;
+                    + "&chat_room_id=" + chatRoomId;
 
             // HTTP request 만들기
             HttpRequest request = HttpRequest.newBuilder()
@@ -91,12 +75,12 @@ public class ChatMessageService {
                     .build();
 
             log.info(request.toString()+ "\n" + request.uri());
-            log.info(json.toString());
             // 보내고 답변받기
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
             // 답변 예외처리
             if (response.statusCode() == 200) {
+                ObjectMapper objectMapper = new ObjectMapper();
                 ObjectNode responseJson = objectMapper.readValue(response.body(), ObjectNode.class);
                 return responseJson.get("message").asText();
             } else {
@@ -108,19 +92,5 @@ public class ChatMessageService {
             log.error("Exception occurred: " + e.getMessage(), e);
             throw new RuntimeException("잘못된 메세지", e);
         }
-    }
-
-    public List<ChatMessageHistoryDto> getChatHistory(Integer chatRoomId){
-        List<ChatMessage> chatMessageList = chatMessageRepository.findByChatRoomId(chatRoomId);
-        List<ChatMessageHistoryDto> chatMessageHistoryDtoList = new ArrayList<>();
-
-        for (ChatMessage chatMessage : chatMessageList){
-            ChatMessageHistoryDto dto = ChatMessageHistoryDto.builder()
-                    .role(chatMessage.getRole() == 0 ? "자녀" : "부모")
-                    .message(chatMessage.getMessage())
-                    .build();
-            chatMessageHistoryDtoList.add(dto);
-        }
-        return chatMessageHistoryDtoList;
     }
 }
